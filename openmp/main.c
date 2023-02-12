7c2b88a571a2bf09a11be524cf6c7acd10a4312e
destrex271 <destrex271@gmail.com>
2023-02-12 11:35:46 +0530

    race_condition

--- openmp/main.c
+++ openmp/main.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 int main(){
+  int numThreads, threadId;// Gets number of threads
   #pragma omp parallel
   {
-    int numThreads = omp_get_num_threads(); // Gets number of threads
-    int threadId = omp_get_thread_num();
+    numThreads = omp_get_num_threads();
+    threadId = omp_get_thread_num();
     // Both present in stack of each thread
     // Each thread has its own copy of numThreads and ThreadId
     printf("Hello from thread %d of %d\n", threadId, numThreads);
